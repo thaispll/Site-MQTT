e68f79f7a8a19d5b4accacacee1d52aaa5863790
Thais De Paula Leal Cobucci <tcobucci@firjan.com.br>
2025-10-29 16:53:50 -0300

    Atualizações do server

---
+++ server.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify, send_from_directory
+import paho.mqtt.client as mqtt
+from threading import Lock
+
+app = Flask(__name__)
+
+broker = "broker.hivemq.com" 
+#broker: servidor que atua como intermediário
+topico = "simulador/mqtt/mensagens" 
+#tópico:Categoria para troca de mensagens
+mensagens = []
+lock = Lock() #objeto de sincronização
+
+
+#configurar o cliente MQTT
+cliente = mqtt.Client()
+
+def on_connect(cliente, userdata, flags, rc):
+    print(f"Conectado ao broker MQTT com código {rc}")
+    cliente.subscribe(topico) #subscribe: assinatura
+
+def on_message(client, userdata, msg):
+    msg_text = msg.payload.decode()
+    with lock:
+        mensagens.append(msg_text)
+        if len(mensagens) > 50: #limita o tamanho da lista para 50 msgs
+            mensagens.pop(0)
+
+cliente.on_connect = on_connect
+cliente.on_message = on_message
+cliente.connect(broker) #conecta ao broker MQTT
+cliente.loop_start()
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html') 
+
+@app.route('/style.css')
+def css():
+    return send_from_directory('.', 'style.css')
+
+@app.route('/enviar',methods=['POST'])
+def enviar():
+    data = request.json
+    msg = data.get('mensagem')
+    
+
+
